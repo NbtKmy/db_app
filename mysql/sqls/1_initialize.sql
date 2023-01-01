b43fbc0e44ce2b21a4d3e2b55e993798008021b1
NbtKmy <nobuta77to99@hotmail.com>
2023-01-01 19:24:11 +0100

    changes in mysql-docker

--- mysql/sqls/1_initialize.sql
+++ mysql/sqls/1_initialize.sql
@@ -1,16 +1,34 @@
-/* test initlialization */
-DROP TABLE IF EXISTS `account`;
+/* initlialization */
+CREATE DATABASE IF NOT EXISTS dblist;
+DROP TABLE IF EXISTS dblist.creator;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `account` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `account_name` varchar(64) NOT NULL UNIQUE,
-  `start_on` datetime NOT NULL,
-  `end_on` datetime NOT NULL,
-  `created_by` int DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_by` int DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `status` int NOT NULL,
+CREATE TABLE dblist.creator (
+  `id` varchar(16) NOT NULL NOT NULL UNIQUE,
+  `name_ja` varchar(128) NOT NULL,
+  `name_en` varchar(128),
+  `geo` POINT,
+  `altnames` varchar(256),
+  `wididata_id` varchar(32),
+  `change_date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS dblist.database;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE dblist.database (
+  `id` varchar(16) NOT NULL UNIQUE,
+  `title_ja` varchar(256) NOT NULL,
+  `title_en` varchar(256),
+  `creator_id` varchar(32),
+  `ddc_category` varchar(128),
+  `type` varchar(128),
+  `description_ja` varchar(1024),
+  `description_en` varchar(1024),
+  `url` varchar(128) NOT NULL,
+  `change_date` date NOT NULL,
+  `link_check` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- mysql/sqls/2_injest.sh
+++ mysql/sqls/2_injest.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 
-mysql -uroot -pmysql --local-infile sada -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/data.csv' INTO TABLE masashi FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n'"
+curl -L 'https://docs.google.com/spreadsheets/d/1WSswTgqGPhKslY4iDD4Z-ZqCP3Il6zV7kNQJVzRdwJw/export?format=csv&gid=0' -o /docker-entrypoint-initdb.d/creator.csv
+curl -L 'https://docs.google.com/spreadsheets/d/14YoKQgpIlIWQHrInwvs3w-SqnowpNMuXGRxZRZxRQxc/export?format=csv&gid=0' -o /docker-entrypoint-initdb.d/database.csv
+
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --local-infile dblist -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/creator.csv' INTO TABLE creator FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES"
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --local-infile dblist -e "LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/database.csv' INTO TABLE database FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES"
