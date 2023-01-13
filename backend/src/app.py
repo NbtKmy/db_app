0d4bea77511f393623c3befd33be1c0ae2b1c367
NbtKmy <nobuta77to99@hotmail.com>
2023-01-13 12:41:35 +0100

    changes in backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,9 +3,11 @@ from flask_restful import Api
 from src.database import init_db
 from src.apis.creator import CreatorAllAPI, CreatorAPI
 from src.apis.databaselist import DatabaselistAllAPI, DatabaselistAPI
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('src.config.Config')
 
     init_db(app)
